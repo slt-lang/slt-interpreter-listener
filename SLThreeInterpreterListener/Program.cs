8aba2a1f2dbce266a4ea009ca5aae51cbafc3f42
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2026-02-04 21:15:05 +0300

    simple server

---
+++ SLThreeInterpreterListener/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SLThreeInterpreterListener
+{
+    public class InvokeRequest
+    {
+        public string Code { get; set; }
+    }
+
+    public class InvokeResponse
+    {
+        public object Result { get; set; }
+    }
+
+    public class Program
+    {
+        /// <summary>
+        /// For example: 8080 5 3600
+        /// </summary>
+        /// <param name="args">PORT TIMEOUT TTL</param>
+        static async Task Main(string[] args)
+        {
+            var server = new HttpServer(new[] { $"http://localhost:{args[0]}/" });
+            var timeout = TimeSpan.FromSeconds(int.Parse(args[1]));
+            var ttl = TimeSpan.FromSeconds(int.Parse(args[2]));
+            Task.Run(() =>
+            {
+                var sw = Stopwatch.StartNew();
+                while (sw.Elapsed < ttl) Thread.Sleep(5000);
+                Environment.Exit(0);
+            });
+
+            var slt_context = new SLThree.ExecutionContext();
+            var parser = new SLThree.Language.Parser();
+
+            server.RegisterEndpoint("POST", "/invoke", async (context) =>
+            {
+                var request = await HttpServer.ReadJsonAsync<InvokeRequest>(context);
+
+
+                var result = default(object);
+                var success = false;
+
+                var task = new Task(() =>
+                {
+                    var sw = Stopwatch.StartNew();
+                    while (!success && sw.Elapsed < timeout) Thread.Sleep(50);
+                    sw.Stop();
+                    if (!success) Environment.Exit(0);
+                });
+                task.Start();
+
+                await Task.WhenAny(task, Task.Run(() =>
+                {
+                    try
+                    {
+                        SLThree.ExecutionContext.IExecutable obj;
+                        try
+                        {
+                            obj = parser.ParseExpression(request.Code);
+                        }
+                        catch
+                        {
+                            obj = parser.ParseScript(request.Code);
+                        }
+
+                        try
+                        {
+                            result = obj.GetValue(slt_context);
+                        }
+                        catch (Exception e)
+                        {
+                            result = e.ToString();
+                        }
+                        finally
+                        {
+                            success = true;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        result = e.ToString();
+                        success = true;
+                    }
+                }));
+
+                var response = new InvokeResponse() { Result = result };
+                await HttpServer.WriteJsonAsync(context, response);
+            });
+
+            var serverTask = server.StartAsync();
+            await serverTask;
+
+            server.Stop();
+        }
+    }
+}
---
+++ SLThreeInterpreterListener/Server.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SLThreeInterpreterListener
+{
+    public class HttpServer
+    {
+        private readonly HttpListener _listener;
+        private readonly Dictionary<string, Func<HttpListenerContext, Task>> _routes;
+        private bool _isRunning;
+
+        public HttpServer(string[] prefixes)
+        {
+            if (!HttpListener.IsSupported)
+                throw new NotSupportedException();
+
+            _listener = new HttpListener();
+            foreach (var prefix in prefixes)
+            {
+                _listener.Prefixes.Add(prefix);
+            }
+
+            _routes = new Dictionary<string, Func<HttpListenerContext, Task>>();
+        }
+
+        public void RegisterEndpoint(string method, string path, Func<HttpListenerContext, Task> handler)
+        {
+            string key = $"{method.ToUpper()} {path.ToLower()}";
+            _routes[key] = handler;
+        }
+
+        public async Task StartAsync()
+        {
+            _listener.Start();
+            _isRunning = true;
+            Console.WriteLine("Server started...");
+
+            while (_isRunning)
+            {
+                try
+                {
+                    var context = await _listener.GetContextAsync();
+                    _ = Task.Run(() => HandleRequestAsync(context));
+                }
+                catch (HttpListenerException) when (!_isRunning)
+                {
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            _isRunning = false;
+            _listener.Stop();
+            _listener.Close();
+        }
+
+        private async Task HandleRequestAsync(HttpListenerContext context)
+        {
+            try
+            {
+                string method = context.Request.HttpMethod;
+                string path = context.Request.Url.AbsolutePath.ToLower();
+                string key = $"{method} {path}";
+
+                if (_routes.ContainsKey(key))
+                {
+                    await _routes[key](context);
+                }
+                else
+                {
+                    context.Response.StatusCode = 404;
+                    context.Response.OutputStream.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка обработки запроса: {ex}");
+                context.Response.StatusCode = 500;
+                await WriteJsonAsync(context, new { error = ex.Message });
+            }
+        }
+
+        public static async Task WriteJsonAsync(HttpListenerContext context, object data)
+        {
+            context.Response.StatusCode = 200;
+            context.Response.ContentType = "application/json";
+            context.Response.ContentEncoding = Encoding.UTF8;
+
+            var json = JsonConvert.SerializeObject(data);
+            var buffer = Encoding.UTF8.GetBytes(json);
+
+            context.Response.ContentLength64 = buffer.Length;
+            await context.Response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+            context.Response.OutputStream.Close();
+        }
+
+        public static async Task<T> ReadJsonAsync<T>(HttpListenerContext context)
+        {
+            using (var reader = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding))
+            {
+                var body = await reader.ReadToEndAsync();
+                return JsonConvert.DeserializeObject<T>(body);
+            }
+        }
+    }
+}
